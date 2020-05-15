4297bbb66530318792407003a0d1bb51ae8e8e18
Torben Schinke <torben.schinke@worldiety.de>
2020-05-15 17:04:35 +0200

    converted sql to ansi standard quotes

--- service/sms/repository.go
+++ service/sms/repository.go
@@ -12,21 +12,25 @@ type SMS struct {
 	Text      string    `ee.sql.Name:"text"`
 }
 
-// @ee.sql.Schema("""
-//  "dialect":"mysql", "value":
-//  "CREATE TABLE IF NOT EXISTS `sms` (
-//  `id` BINARY(16) NOT NULL COMMENT 'uuid of the sms',
-//  `recipient` VARCHAR(255) NOT NULL COMMENT 'the phone number to send to',
-//  `text` TEXT NOT NULL COMMENT 'SMS text per message is limited but can be joined from an arbitrary amount.',
-//  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'the time this sms has been created',
-//  `send_at` TIMESTAMP NOT NULL DEFAULT 0 COMMENT 'the time this sms has been sent or 0',
-//  `status` ENUM('unknown', 'success', 'failed') NOT NULL DEFAULT 'unknown' COMMENT 'the current status',
-//  `details` JSON NOT NULL DEFAULT '{}' COMMENT 'contains arbitrary status details',
-//  PRIMARY KEY (`id`))
-// ENGINE = InnoDB;"
-// """)
-//
-// @ee.stereotype.Repository("sms")
+/**
+@ee.sql.Schema("""
+	{
+		"dialect":"mysql"
+	}
+	CREATE TABLE IF NOT EXISTS "sms" (
+	  "id" BINARY(16) NOT NULL,
+	  "recipient" VARCHAR(255) NOT NULL COMMENT 'the phone number to send to',
+	  "text" TEXT NOT NULL COMMENT 'SMS text is limited to 160 chars (non multibyte?) per message but can be joined from an arbitrary amount of sms messages.',
+	  "created_at" TIMESTAMP NOT NULL,
+	  "send_at" TIMESTAMP NOT NULL DEFAULT 0,
+	  "status" ENUM('unknown', 'success', 'failed') NOT NULL DEFAULT 'unknown',
+	  "details" JSON NOT NULL DEFAULT '{}' COMMENT 'contains arbitrary status details',
+	  PRIMARY KEY ("id"))
+	ENGINE = InnoDB;
+""")
+
+@ee.stereotype.Repository("sms")
+ */
 type Repository interface {
 	// @ee.sql.Query("SELECT id,created_at,text FROM sms LIMIT :limit")
 	FindAll(ctx context.Context, limit int) ([]SMS, error)
--- service/sms/rest.go
+++ service/sms/rest.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"github.com/golangee/uuid"
+	"net/http"
 )
 
 // @ee.http.Controller
@@ -23,9 +24,15 @@ func (s *RestController) List(ctx context.Context, limit int) ([]SMS, error) {
 }
 
 // @ee.http.HeaderParam("value":"userAgent","alias":"User-Agent")
-// @ee.http.Route("/:id")
+// @ee.http.Route("/a/:id")
 // @ee.http.Method("GET")
 func (s *RestController) Get(ctx context.Context, id uuid.UUID, userAgent string) (SMS, error) {
 	fmt.Println(userAgent)
 	return s.sms.FindById(ctx, id)
+}
+
+// @ee.http.Route("/asdf")
+// @ee.http.Method("GET")
+func (s *RestController) Get2(res http.ResponseWriter, req *http.Request) {
+	res.Write([]byte("hello world"))
 }
